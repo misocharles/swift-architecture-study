e5bae06db0654b619749af4988fd33e2cae6635f
misocharles <misocharles@gmail.com>
2017-07-31 17:58:02 +0900

    Update github 이슈 목록 노출
    - 이슈의 title, number
    - https://api.github.com/repos/lkzhao/Hero/issues

--- ArchitectureStudy/ArchitectureStudy/ViewController.swift
+++ ArchitectureStudy/ArchitectureStudy/ViewController.swift
@@ -7,19 +7,49 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    var issueData: [Issue] = [Issue]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        requestIssues()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return issueData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "IssueCell", for: indexPath)
+        cell.textLabel?.text = issueData[indexPath.row].title
+        cell.detailTextLabel?.text = "#\(issueData[indexPath.row].number)"
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate  {
+    
+}
+
+extension ViewController {
+    func requestIssues() {
+        let urlString = "https://api.github.com/repos/lkzhao/Hero/issues"
+        Alamofire.request(urlString).responseJSON { (response) in
+            if let json = response.result.value as? [Dictionary<String, Any>] {
+                for data in json {
+                    if let title = data["title"] as? String, let number = data["number"] as? Int {
+                        let issue = Issue(title: title, number: number)
+                        self.issueData.append(issue)
+                    }
+                }
+                self.tableView.reloadData()
+            }
+        }
+    }
+}
