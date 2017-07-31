3ca853191591f538d51bbabd75b89d1df20feb26
misocharles <misocharles@gmail.com>
2017-07-31 16:44:52 +0900

    Create Architecture Study Project
    - Alamofire 추가

---
+++ ArchitectureStudy/ArchitectureStudy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ArchitectureStudy
+//
+//  Created by Incheol-iMac on 2017. 7. 31..
+//  Copyright © 2017년 misocharles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ArchitectureStudy/Pods/Target Support Files/Pods-ArchitectureStudyTests/Pods-ArchitectureStudyTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ArchitectureStudyTests : NSObject
+@end
+@implementation PodsDummy_Pods_ArchitectureStudyTests
+@end
